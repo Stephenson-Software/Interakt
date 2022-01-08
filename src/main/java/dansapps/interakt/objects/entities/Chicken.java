e785b9c36046806d910f8dc499f4b5253c89f925
dmccoystephenson <dmccoystephenson@gmail.com>
2022-01-07 23:25:19 -0700

    Created Chicken.java and Forest.java

---
+++ src/main/java/dansapps/interakt/objects/entities/Chicken.java
@@ -0,0 +1,14 @@
+package dansapps.interakt.objects.entities;
+
+import dansapps.interakt.objects.domain.Entity;
+
+public class Chicken extends Entity {
+
+    /**
+     * @author Daniel Stephenson
+     * @since January 7th, 2022
+     */
+    public Chicken(int ID, String name) {
+        super(ID, name);
+    }
+}
---
+++ src/main/java/dansapps/interakt/objects/environments/Forest.java
@@ -0,0 +1,14 @@
+package dansapps.interakt.objects.environments;
+
+import dansapps.interakt.objects.domain.Environment;
+
+public class Forest extends Environment {
+
+    /**
+     * @author Daniel Stephenson
+     * @since January 7th, 2022
+     */
+    public Forest(int ID, String name, int size) {
+        super(ID, name, size);
+    }
+}
