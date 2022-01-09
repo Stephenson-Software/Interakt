13245c3f7617a0a2b7318ff98705eeb2868d854e
dmccoystephenson <dmccoystephenson@gmail.com>
2022-01-08 23:54:12 -0700

    Created Move.java and deleted Befriend.java

---
+++ src/main/java/dansapps/interakt/objects/actions/Move.java
@@ -0,0 +1,10 @@
+package dansapps.interakt.objects.actions;
+
+import dansapps.interakt.objects.abs.Action;
+
+public class Move extends Action {
+
+    public Move() {
+        super("Move");
+    }
+}
