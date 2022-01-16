80106dd10bb650330a57e4fc7a3634a7e94b123a
dmccoystephenson <dmccoystephenson@gmail.com>
2022-01-15 23:19:14 -0700

    Altered some config settings and added some annotations.

--- src/main/java/dansapps/interakt/misc/CONFIG.java
+++ src/main/java/dansapps/interakt/misc/CONFIG.java
@@ -6,6 +6,6 @@ package dansapps.interakt.misc;
  */
 public class CONFIG {
     public static final int TIME_SLOT_LENGTH_IN_SECONDS = 60;
-    public static final int GRID_SIZE = 10;
-    public static final GRID_DISPLAY_TYPE DISPLAY_TYPE = GRID_DISPLAY_TYPE.CHARACTER_AT_INDEX_ZERO;
+    public static final int GRID_SIZE = 4;
+    public static final GRID_DISPLAY_TYPE DISPLAY_TYPE = GRID_DISPLAY_TYPE.NUMBER_OF_ENTITIES;
 }
--- src/main/java/dansapps/interakt/misc/GRID_DISPLAY_TYPE.java
+++ src/main/java/dansapps/interakt/misc/GRID_DISPLAY_TYPE.java
@@ -1,5 +1,9 @@
 package dansapps.interakt.misc;
 
+/**
+ * @author Daniel McCoy Stephenson
+ * @since January 15th, 2022
+ */
 public enum GRID_DISPLAY_TYPE {
     SIMPLE,
     CHARACTER_AT_INDEX_ZERO,
