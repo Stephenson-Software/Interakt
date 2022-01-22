ec7d430b853848173abe4b7b22a0a84e610022e1
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2022-01-22 00:12:35 -0700

    Health (#46)
    * Altered a logging message and the CONFIG class.
    
    * Made actors have a certain amount of health.

--- src/main/java/dansapps/interakt/misc/CONFIG.java
+++ src/main/java/dansapps/interakt/misc/CONFIG.java
@@ -10,5 +10,6 @@ public class CONFIG {
     public static final int GRID_SIZE = 4;
     public static final GRID_DISPLAY_TYPE DISPLAY_TYPE = GRID_DISPLAY_TYPE.NUMBER_OF_ENTITIES;
     public static final int MAX_CHANCE_TO_MOVE = 25;
-    public static final int SECONDS_BETWEEN_AUTO_SAVES = 60;
+    public static final int SECONDS_BETWEEN_AUTO_SAVES = 600;
+    public static final double MAX_HEALTH = 100;
 }
