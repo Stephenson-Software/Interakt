347c165a31869f9d39c29adaf53d4da02434e70c
dmccoystephenson <dmccoystephenson@gmail.com>
2022-03-19 15:26:32 -0600

    Altered default config options.

--- src/main/java/dansapps/interakt/misc/CONFIG.java
+++ src/main/java/dansapps/interakt/misc/CONFIG.java
@@ -6,13 +6,13 @@ package dansapps.interakt.misc;
  */
 public class CONFIG {
     public static boolean DEBUG_FLAG = false;
-    public static final int TIME_SLOT_LENGTH_IN_SECONDS = 6;
-    public static final int GRID_SIZE = 4;
+    public static final int TIME_SLOT_LENGTH_IN_SECONDS = 60;
+    public static final int GRID_SIZE = 8;
     public static final int MAX_CHANCE_TO_MOVE = 25;
     public static final int MAX_CHANCE_TO_BEFRIEND = 50;
     public static final int MAX_CHANCE_TO_ATTACK = 50;
     public static final int SECONDS_BETWEEN_AUTO_SAVES = 300;
     public static final double MAX_HEALTH = 100;
     public static final double MAX_DAMAGE = 50;
-    public static final GRID_DISPLAY_TYPE DISPLAY_TYPE = GRID_DISPLAY_TYPE.NUMBER_OF_ENTITIES;
+    public static final GRID_DISPLAY_TYPE DISPLAY_TYPE = GRID_DISPLAY_TYPE.CHARACTER_AT_INDEX_ZERO;
 }
