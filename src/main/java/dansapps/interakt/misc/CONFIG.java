9a5d5bad7c8ceb69e22cc1c96e21b488d259baf1
dmccoystephenson <dmccoystephenson@gmail.com>
2022-05-25 21:09:05 -0600

    Modified max damage.

--- src/main/java/dansapps/interakt/misc/CONFIG.java
+++ src/main/java/dansapps/interakt/misc/CONFIG.java
@@ -14,7 +14,7 @@ public class CONFIG {
     public static final int MAX_CHANCE_TO_REPRODUCE = 25;
     public static final int SECONDS_BETWEEN_AUTO_SAVES = 300;
     public static final double MAX_HEALTH = 100;
-    public static final double MAX_DAMAGE = 50;
+    public static final double MAX_DAMAGE = 10;
     public static final GRID_DISPLAY_TYPE DISPLAY_TYPE = GRID_DISPLAY_TYPE.CHARACTER_AT_INDEX_ZERO;
     public static final boolean SHOW_LINEAGE_INFO = true;
     public static final boolean LOG_EVENTS_TO_CONSOLE = false;
