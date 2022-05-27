a16b63360aceecee7bfa5e124e544d78cb784005
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2022-05-27 00:15:50 -0600

    @Since Annotation Fix (#89)
    * Renamed the utility class that is used by the tests.
    
    * Altered a @since annotation.

--- src/main/java/dansapps/interakt/misc/enums/ACTION_TYPE.java
+++ src/main/java/dansapps/interakt/misc/enums/ACTION_TYPE.java
@@ -2,7 +2,7 @@ package dansapps.interakt.misc.enums;
 
 /**
  * @author Daniel McCoy Stephenson
- * @since January 15th, 2022
+ * @since January 27th, 2022
  */
 public enum ACTION_TYPE {
     ATTACK,
